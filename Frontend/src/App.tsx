36cc4ea12955397c9ef4166946b78e920715ba66
Aayush Prasai <gayatriprasai@gmail.com>
2026-01-04 07:39:21 +0545

    Add Simple UI of BUYER  Dashboard

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Login, Signup } from "./pages/auth";
 import { AdminDashboard } from "./pages/dashboard/admin/AdminDashboard";
+import { BuyerDashboard } from "./pages/dashboard/buyer/BuyerDashboard";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard/admin" element={<AdminDashboard />} />
+        <Route path="/dashboard/buyer" element={<BuyerDashboard />} />
       </Routes>
     </Router>
   );
