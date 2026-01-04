678f16674ecdb73f54d8fe0fdca489a061eb94ca
Aayush Prasai <gayatriprasai@gmail.com>
2026-01-04 19:22:44 +0545

    Fix Forgot Password Route, - Dashboard sidebar ffix in Buyerdashboard

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Login, Signup } from "./pages/auth";
+import { ForgotPassword, Login, Signup } from "./pages/auth";
 import { AdminDashboard } from "./pages/dashboard/admin/AdminDashboard";
 import { BuyerDashboard } from "./pages/dashboard/buyer/BuyerDashboard";
 import { SellerDashboard } from "./pages/dashboard/seller/SellerDashboard";
@@ -12,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/dashboard/admin" element={<AdminDashboard />} />
         <Route path="/dashboard/buyer" element={<BuyerDashboard />} />
         <Route path="/dashboard/seller" element={<SellerDashboard />} />
